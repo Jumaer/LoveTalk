0375675289961726162bd5a8b950599523d33d89
Jumaer <jumaerahamedcse.bd.com@gmail.com>
2020-01-14 15:10:52 -0800

    Recommit chat actvity

--- app/src/main/java/com/example/lovetalk/ChatActivity.java
+++ app/src/main/java/com/example/lovetalk/ChatActivity.java
@@ -1,14 +1,37 @@
 package com.example.lovetalk;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.lovetalk.chat.MessageAdapter;
+import com.example.lovetalk.chat.ChatObject;
+import com.example.lovetalk.chat.MessageObject;
+
+import java.util.ArrayList;
+
 public class ChatActivity extends AppCompatActivity {
 
+    private RecyclerView mChat;
+    private RecyclerView.Adapter mChatAdapter;
+    private RecyclerView.LayoutManager mChatLayoutManager;
+    ArrayList<MessageObject> messageList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        initializeRecyclerView();
+    }
+    private void initializeRecyclerView(){
+        messageList = new ArrayList<>();
+        mChat = findViewById(R.id.recycle_message);
+        mChat.setNestedScrollingEnabled(false);
+        mChat.setHasFixedSize(true);
+        mChatLayoutManager = new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.VERTICAL,false);
+        mChat.setLayoutManager(mChatLayoutManager);
+        mChatAdapter = new MessageAdapter(messageList);
+        mChat.setAdapter(mChatAdapter);
     }
 }
--- app/src/main/java/com/example/lovetalk/chat/MessageAdapter.java
+++ app/src/main/java/com/example/lovetalk/chat/MessageAdapter.java
@@ -1,4 +1,59 @@
 package com.example.lovetalk.chat;
 
-public class MessageAdapter {
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.lovetalk.R;
+
+import java.util.ArrayList;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageRecyclerViewHolder> {
+    public Context context;
+    ArrayList<MessageObject> message;
+    public MessageAdapter(ArrayList<MessageObject>message) {
+        this.message = message;
+    }
+
+    @NonNull
+    @Override
+    public MessageRecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View layout_view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message,null,false);
+        RecyclerView.LayoutParams lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        layout_view.setLayoutParams(lp);
+        MessageRecyclerViewHolder rcv = new MessageRecyclerViewHolder(layout_view);
+        return rcv;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageAdapter.MessageRecyclerViewHolder holder, final int position) {
+  holder.mMessage.setText(message.get(position).getMessage());
+        holder.mSender.setText(message.get(position).getSenderId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return message.size();
+    }
+
+    public class MessageRecyclerViewHolder extends RecyclerView.ViewHolder {
+         LinearLayout mlayout;
+        TextView mMessage,mSender;
+
+        MessageRecyclerViewHolder(View v){
+            super(v);
+         //   mlayout = v.findViewById(R.id.layout);
+            mMessage = v.findViewById(R.id.message);
+            mSender=v.findViewById(R.id.sender);
+
+        }
+    }
 }
+
